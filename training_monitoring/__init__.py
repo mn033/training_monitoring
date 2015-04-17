4ccd50ce8e7bbf9699da29609aeb67aebe95cfa0
jgru <jang92@gmx.de>
2015-04-17 13:58:30 +0200

    Updated __init__.py

--- training_monitoring/__init__.py
+++ training_monitoring/__init__.py
@@ -7,7 +7,7 @@ __version__ = '0.1'
 __author__ = 'jgru'
 
 
-from parsers import TrainingDataParser, XlsParser
-from plotter import Plotter
-from training_constants import *
-from training_monitor import create_training_stats
+from .parsers import TrainingDataParser, XlsParser
+from .plotter import Plotter
+from .training_constants import *
+from .training_monitor import create_training_stats
