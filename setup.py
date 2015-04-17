922b20f98f683b0d48c41acad88b934ee57e2ac3
jgru <jang92@gmx.de>
2015-04-17 12:56:10 +0200

    Prepared as python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='training_monitoring',
+      version='0.1',
+      description='A tool to visualize training data',
+      url='https://github.com/jgru/training_monitoring',
+      author='jgru',
+      author_email='',
+      packages=['training_monitoring'],
+      zip_safe=False)
