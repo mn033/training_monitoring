c3ac223848bcf9d7865c95b4db982aaa58d199d8
jgru <jang92@gmx.de>
2015-04-17 13:06:07 +0200

    Corrected setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='training_monitoring',
       description='A tool to visualize training data',
       url='https://github.com/jgru/training_monitoring',
       author='jgru',
-      author_email='',
+      author_email='jang92@gmx.de',
       packages=['training_monitoring'],
       zip_safe=False)
